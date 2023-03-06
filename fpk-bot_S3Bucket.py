7d6e4a817b155bae3fe00ca8c33b4f26a9b18d09
Achraf Mansari <contact@achrafmansari.com>
2023-03-06 14:47:30 +0100

    updated the script to be compatible with the new college website

--- fpk-bot_S3Bucket.py
+++ fpk-bot_S3Bucket.py
@@ -9,18 +9,25 @@ from selenium.webdriver.chrome.options import Options
 
 
 def lambda_handler(_event, _context):
+
+    # Environment Variables
     url1 = os.getenv('URL1')
-    url2 = os.getenv('URL2')
+    bucket = os.getenv('BUCKET_NAME')
+    storage_file = os.getenv('STORAGE_FILE')
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    TELEGRAM_CHAT_ID_TEST = os.getenv('TELEGRAM_CHAT_ID_TEST')
+
+    if TELEGRAM_TOKEN is None:
+        raise EnvironmentError("XXXXXXXXXXXXXXXXXX Missing TELEGRAM_TOKEN env variable! XXXXXXXXXXXXXXXXXX")
+
     try:
         college_website_response = requests.get(url1, timeout=6).status_code
         if college_website_response == 200:
             # ████████████████████████████████████████████ S3 BUCKET 1 ████████████████████████████████████████████
             s3 = boto3.client('s3')
-            bucket = os.getenv('BUKCET_NAME')
-            storage_file = os.getenv('STORAGE_FILE')
-            new_announcements = {}
             old_announcements = json.loads(
-            s3.get_object(Bucket=bucket, Key=storage_file)["Body"].read().decode('utf-8'))
+                s3.get_object(Bucket=bucket, Key=storage_file)["Body"].read().decode('utf-8')
+            )
 
             # ████████████████████████████████████████████ WEB SCRAPPING ████████████████████████████████████████████
             # Setup the web scrapper -------------------------------------------------
@@ -30,55 +37,46 @@ def lambda_handler(_event, _context):
             options.add_argument('--no-sandbox')
             options.add_argument('--single-process')
             options.add_argument('--disable-dev-shm-usage')
-
             web_page = webdriver.Chrome('/opt/chromedriver', chrome_options=options)
+            web_page.get(url1)
 
-            # Scrap all the current announcments from the website -------------------------------------------------
+            # Scrap all the current announcements from the website -------------------------------------------------
             current_announcements = {}
 
-            web_page1 = webdriver.Chrome('/opt/chromedriver', chrome_options=options)
-            web_page1.get(url1)
-            table = web_page1.find_element_by_tag_name('tbody').find_elements_by_tag_name("tr")
-            for row in table:
-                col = row.find_element_by_tag_name('td')
+            articles = web_page.find_element_by_id("main").find_elements_by_tag_name("article")
+            for article in articles:
 
-                lien = col.find_element_by_tag_name('a').get_attribute('href').replace("index.php/", "")
-                titre = str(col.find_elements_by_tag_name("a")[0].text)
-                date = str(col.text).replace(titre, "").replace("\n>> Lire la suite", "").replace(" -", "")
+                # Extract the title and link
+                header = article.find_element_by_tag_name('header')
+                h2 = header.find_element_by_tag_name('h2')
+                a_link1 = h2.find_element_by_tag_name("a")
+                link = a_link1.get_attribute('href')
+                title = a_link1.text
+
+                # Extract the date
+                div_entry_meta = header.find_element_by_tag_name("div")
+                span = div_entry_meta.find_element_by_tag_name("span")
+                a_link2 = span.find_element_by_tag_name("a")
+                date = a_link2.find_element_by_tag_name("time").text
 
                 # Storing the current announcements in a list
-                current_announcements[lien] = {
-                    'titre': titre,
+                current_announcements[link] = {
+                    'titre': title,
                     'date': date
                 }
-            web_page1.close()
-            web_page1.quit()
 
-            web_page2 = webdriver.Chrome('/opt/chromedriver', chrome_options=options)
-            web_page.get(url2)
-            table = web_page.find_element_by_tag_name('tbody').find_elements_by_tag_name("tr")
-            for row in table:
-                cols = row.find_elements_by_tag_name('td')
-
-                date = cols[0].text
-                titre = cols[1].find_element_by_tag_name('a').text
-                lien = cols[1].find_element_by_tag_name('a').get_attribute('href').replace("index.php/", "")
-
-                # Storing the current announcements in a list
-                if lien not in current_announcements:
-                    current_announcements[lien] = {
-                        'titre': titre,
-                        'date': date
-                    }
-            web_page2.close()
-            web_page2.quit()
+            web_page.close()
+            web_page.quit()
 
             # ████████████████████████████████████████████ S3 BUCkET 2 ████████████████████████████████████████████
-            for current_announcement in current_announcements:
-                if current_announcement.replace("index.php/", "") not in {**old_announcements, **new_announcements}:
-                    new_announcements[current_announcement.replace("index.php/", "")] = {
-                        'titre': current_announcements[current_announcement]['titre'],
-                        'date': current_announcements[current_announcement]['date'],
+            new_announcements = {}
+            for announcement_link in current_announcements:
+                if announcement_link not in {**old_announcements, **new_announcements}:
+
+                    # Add any new announcement to the new_announcements dictionary
+                    new_announcements[announcement_link] = {
+                        'titre': current_announcements[announcement_link]['titre'],
+                        'date': current_announcements[announcement_link]['date'],
                     }
 
             if len(new_announcements) > 0:
@@ -86,22 +84,16 @@ def lambda_handler(_event, _context):
                 s3.put_object(Bucket=bucket, Key=storage_file, Body=current_announcements_json)
 
                 # ████████████████████████████████████████████ BOT ████████████████████████████████████████████
-                bot_token = os.getenv('TELEGRAM_TOKEN')
-                bot = telegram.Bot(bot_token)
-                chat_id = -int(os.getenv('TELEGRAM_CHAT_ID_TEST'))
 
-                def send_message(chat_id, message_content):
-                    # Check that token is not empty
-                    if bot_token is not None:
-                        bot.send_message(text=message_content, chat_id=chat_id, parse_mode=telegram.ParseMode.HTML)
-                    else:
-                        raise EnvironmentError("Missing TELEGRAM_TOKEN env variable!")
+                bot = telegram.Bot(TELEGRAM_TOKEN)
+                chat_id = -int(TELEGRAM_CHAT_ID_TEST)
 
                 for announcement in new_announcements:
                     message = f'''<a href="{announcement}">{new_announcements[announcement]['titre']}</a>\n\nDate : {new_announcements[announcement]['date']}\n\n'''
-                    send_message(chat_id, message)
+                    bot.send_message(text=message, chat_id=chat_id, parse_mode=telegram.ParseMode.HTML)
                     time.sleep(3)
 
             return "████████████████████████████████████████████ SUCCESS ████████████████████████████████████████████"
+
     except requests.exceptions.Timeout:
         return "XXXXXXXXXXXXXXXXXX COLLEGE WEBSITE IS DOWN XXXXXXXXXXXXXXXXXX"
